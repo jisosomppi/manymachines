97af4d5ede861c10fc82af9e4a9060889f5c0648
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-04-18 08:28:28 +0000

    Delete vmkiller.sh

--- vmkiller.sh
+++
@@ -1,4 +0,0 @@
-# Script for destroying all existing Vagrant machines
-# Jussi Isosomppi, 2018
-
-for i in `vagrant global-status | grep virtualbox | awk '{ print $1 }'` ; do vagrant destroy $i ; done
