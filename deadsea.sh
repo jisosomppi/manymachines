57e0ed8975e846aa2b933ed61f0658c3571c436e
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-04-25 11:10:50 +0300

    Update deadsea.sh

--- deadsea.sh
+++ deadsea.sh
@@ -3,14 +3,8 @@
 
 cd
 
-setxkbmap fi
 sudo apt-get update
 sudo apt-get install -y git virtualbox vagrant squid-deb-proxy
-
-git config --global user.name "Jussi Isosomppi"
-git config --global user.email "jussi.isosomppi@gmail.com"
-git config --global credential.helper "cache --timeout=3600"
-git config --global push.default simple
 
 git clone https://github.com/jisosomppi/manymachines
 
