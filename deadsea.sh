26149ec034caa68e91d49da4aa48d9f400c5a010
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-07 18:05:16 +0300

    Create deadsea.sh

---
+++ deadsea.sh
@@ -0,0 +1,13 @@
+## Startup script for setting up bare metal computers as Vagrant+Salt -slave colonies
+## Jussi Isosomppi, 2018
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git vagrant virtualbox ssh
+
+
+# Run script to setup new basebox with updated packages and salt-minion with correct master IP
+bash makebox.sh
+
+# Run script to setup masses of Vagrant VM's using new box
+bash clonebox.sh
