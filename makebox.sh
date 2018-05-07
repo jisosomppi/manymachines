527bb7e05a8d5a7145d4945e4b1b3177b09afcde
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-07 17:03:43 +0300

    Update makebox.sh

--- makebox.sh
+++ makebox.sh
@@ -1,3 +1,9 @@
+## Jussi Isosomppi, 2018
+## Sources:
+## https://scotch.io/tutorials/how-to-create-a-vagrant-base-box-from-an-existing-one
+## https://www.vagrantup.com/docs/virtualbox/boxes.html
+## https://stackoverflow.com/questions/22523134/running-remote-commands-after-vagrant-ssh
+
 cd
 mkdir vagrant
 cd vagrant/
@@ -6,13 +12,13 @@ cd vagrant/
 vagrant init minimal/trusty64
 vagrant up
 
-# run commands inside box
+# customize base box
 vagrant ssh -- -t 'sudo apt-get update; sudo apt-get install -y salt-minion; echo -e "\nmaster: 172.28.171.118\n" | sudo tee -a /etc/salt/minion; sudo apt-get clean; cat /dev/null > ~/.bash_history && history -c && exit'
 
 # create new box and make it usable
 vagrant package --output rdyslave.box
 vagrant box add rdyslave rdyslave.box
 
-# allow use of 
+# remove base box
 vagrant destroy
 rm Vagrantfile
