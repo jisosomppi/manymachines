a84e7e5726589e56a46cd1d24021c3f79ca24b7b
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-08 10:32:27 +0300

    Update clonebox.sh

--- clonebox.sh
+++ clonebox.sh
@@ -1,8 +1,11 @@
 ## Script for setting up masses of Vagrant+salt slaves
 ## Jussi Isosomppi, 2018
 
+
+# move premade Vagrantfile for clones and rename it
 cd
 cp manymachines/Vagrantfile_rdyslave vagrant/Vagrantfile
 cd vagrant/
 
+# start creating 100 VMs
 vagrant up
