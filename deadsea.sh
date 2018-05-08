5b66c8409a70bd4f038196b458eb874f05dbf66d
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-08 09:44:36 +0300

    Update deadsea.sh

--- deadsea.sh
+++ deadsea.sh
@@ -5,11 +5,12 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git vagrant virtualbox ssh
 
+cd
+git clone https://github.com/jisosomppi/manymachines.git
+cd manymachines/
 
 # Run script to setup new basebox with updated packages and salt-minion with correct master IP
-wget https://github.com/jisosomppi/manymachines/raw/master/makebox.sh
 bash makebox.sh
 
 # Run script to setup masses of Vagrant VM's using new box
-wget https://github.com/jisosomppi/manymachines/raw/master/clonebox.sh
 bash clonebox.sh
