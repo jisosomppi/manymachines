f544f50db152de98f4d8873c737602e356b3e30c
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-11 14:03:44 +0300

    Update clonebox.sh

--- clonebox.sh
+++ clonebox.sh
@@ -4,8 +4,8 @@
 
 # move premade Vagrantfile for clones and rename it
 cd
-cp manymachines/Vagrantfile_rdyslave vagrant/Vagrantfile
-cd vagrant/
+cp ~/manymachines/Vagrantfile_rdyslave ~/vagrant/Vagrantfile
+cd ~/vagrant/
 
 # start creating 100 VMs
 vagrant up
